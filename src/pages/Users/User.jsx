6661ce088278d87690dbdc0ea62a38d148b9ef92
izza-sirkast <dawlaizza405@gmail.com>
2025-01-08 15:25:56 +0700

    feat: author / other user page

--- src/pages/Users/User.jsx
+++ src/pages/Users/User.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import api from '../../api/api'
 
 function User() {
+  const { userId } = useParams()
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await api.get(`user/users/${userId}`)
+        setUser(res.data.data)
+      } catch (error) {
+        console.log(error)
+        alert("User not found")
+      }
+    }
+    getUser()
+  }, [])
+
+  if(!user) return <p>Loading...</p>;
+
   return (
-    <div>User</div>
+    <div>
+        <h1 className='text-xl font-bold'>User Detail</h1>
+        <p>Id : {user.id}</p>
+        <p>Name : {user.name}</p>
+        <p>Email : {user.email}</p>
+    </div>
   )
 }
 
