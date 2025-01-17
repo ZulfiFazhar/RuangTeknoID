3f91b014417a628b2ddcfd31c1565207165e9770
ZulfiFazhar <zulfi.fazhar12@gmail.com>
2025-01-17 10:38:30 +0700

    feat: slicing feed post article

---
+++ src/components/post/CommentPost.jsx
@@ -0,0 +1,9 @@
+import ContentEditor from "@/components/editor/ContentEditor";
+
+export default function CommentPost() {
+  return (
+    <div className="w-4/5 m-auto mt-4">
+      <ContentEditor />
+    </div>
+  );
+}
