61ecd715f171faa7d00861f795a70ae80f35f25c
ZulfiFazhar <zulfi.fazhar12@gmail.com>
2025-02-04 20:33:10 +0700

    fix: resolve discussions aspect ratio 😁

--- src/pages/Discussions.jsx
+++ src/pages/Discussions.jsx
@@ -2,7 +2,7 @@ import FeedDiscussions from "@/pages/Discussions/FeedDiscussions";
 
 export default function Discussions() {
   return (
-    <div className="grid w-4/5 m-auto">
+    <div className="grid w-4/5 m-auto mt-0">
       <h1 className="text-2xl font-bold mb-4">Forum Diskusi</h1>
       <div>
         <FeedDiscussions />
